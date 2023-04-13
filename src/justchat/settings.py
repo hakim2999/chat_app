3b82ebc45eca60ef1ea527d16ad84e7fd932ce88
hakim2999 <49701397+hakim2999@users.noreply.github.com>
2023-04-13 11:07:14 +0300

    Update settings.py

--- src/justchat/settings.py
+++ src/justchat/settings.py
@@ -21,8 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ia!!g#om4s64m)@5x_=^ma+r-mh5d2!*-%2+05mvhhsh69_$c%'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
